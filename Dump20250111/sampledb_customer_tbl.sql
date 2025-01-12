e5ffe21f896a3612e085dd3c465034269a19879b
minemint <nathithorn.klomkliao@gmail.com>
2025-01-12 18:44:19 +0700

    modifly sql

--- Dump20250111/sampledb_customer_tbl.sql
+++ Dump20250111/sampledb_customer_tbl.sql
@@ -39,7 +39,7 @@ CREATE TABLE `customer_tbl` (
   KEY `fk_customerSubDistrict` (`subdistrictId`),
   CONSTRAINT `fk_customerDistrict` FOREIGN KEY (`districtId`) REFERENCES `district_tbl` (`districtId`),
   CONSTRAINT `fk_customerSubDistrict` FOREIGN KEY (`subdistrictId`) REFERENCES `subdistrict_tbl` (`subdistrictId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +48,7 @@ CREATE TABLE `customer_tbl` (
 
 LOCK TABLES `customer_tbl` WRITE;
 /*!40000 ALTER TABLE `customer_tbl` DISABLE KEYS */;
-INSERT INTO `customer_tbl` VALUES (1,'mr','min','mint','108/178','1',1,1,11251,NULL,NULL);
+INSERT INTO `customer_tbl` VALUES (1,'Mr.','moodeng','Doe','123 Main St','1',1,1,10110,812345678,NULL),(4,'mrs','min2','mine','108/178','1',2,1,12501,NULL,NULL),(5,'Mr.','John','Doe','123 Main St',NULL,1,1,10110,812345678,'string'),(7,'dad','d','dad','da',NULL,1,1,10500,0,'string');
 /*!40000 ALTER TABLE `customer_tbl` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -61,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-11 20:32:25
+-- Dump completed on 2025-01-12 18:43:27
--- Dump20250111/sampledb_district_tbl.sql
+++ Dump20250111/sampledb_district_tbl.sql
@@ -25,11 +25,8 @@ DROP TABLE IF EXISTS `district_tbl`;
 CREATE TABLE `district_tbl` (
   `districtId` int NOT NULL AUTO_INCREMENT,
   `districtName` varchar(45) DEFAULT NULL,
-  `subdistrictId` int DEFAULT NULL,
-  PRIMARY KEY (`districtId`),
-  KEY `fk_districtSub` (`subdistrictId`),
-  CONSTRAINT `fk_districtSub` FOREIGN KEY (`subdistrictId`) REFERENCES `subdistrict_tbl` (`subdistrictId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`districtId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +35,7 @@ CREATE TABLE `district_tbl` (
 
 LOCK TABLES `district_tbl` WRITE;
 /*!40000 ALTER TABLE `district_tbl` DISABLE KEYS */;
-INSERT INTO `district_tbl` VALUES (1,'D',1);
+INSERT INTO `district_tbl` VALUES (1,'D'),(2,'A');
 /*!40000 ALTER TABLE `district_tbl` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-11 20:32:25
+-- Dump completed on 2025-01-12 18:43:27
